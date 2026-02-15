cd5164afae9b1499abfba226e60abfe2123fe179
Praveen Sundaram <praveensundaram1@gmail.com>
2026-02-15 12:04:26 -0600

    2.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Total = (props) => <p><b>Number of exercises {props.total}</b></p>
-
-
-const Content = ({ parts }) => {
-
-  return (<div>
-    { parts.map( (part) => <Part part={part} /> )}
-    <Total total={ parts.reduce ((sum , part) => sum+part.exercises , 0 )} />
-    </div>
-  )
-}
-
-const Part = ({part}) => {
-  console.log("part is ", part)
-  return (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-}
-
-const Course = ( {course} ) => {
-  console.log("course parts ", JSON.stringify(course.parts));
-  return (
-    <div>
-       <Header course={course.name} />
-       <Content parts={course.parts} />
-    </div>
-  )
-}
+import Course from './Components/Course.jsx'
 
 const App = () => {
   const courses = [
@@ -77,7 +46,7 @@ const App = () => {
     }
   ]
 
-  return <>{ courses.map( (course) => <Course course={course} /> )} </>
+  return <>{ courses.map( (course) => <Course key={course.id} course={course} /> )} </>
 }
 
 export default App
