540c32e1f9c679443941de9b8449188ed2542c43
Praveen Sundaram <praveensundaram1@gmail.com>
2026-02-17 19:48:19 -0600

    2.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,52 +1,46 @@
-import Course from './Components/Course.jsx'
+import { useState } from "react";
+
+const Person = ({ person }) => (
+  <div>{person.name}</div>
+);
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Arto Hellas" }
+  ]);
 
-  return <>{ courses.map( (course) => <Course key={course.id} course={course} /> )} </>
-}
+  const [newName, setNewName] = useState("");
 
-export default App
+  const nameOnChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const newNameOnSubmit = (event) => {
+    event.preventDefault();
+
+    const person = {
+      id: persons.length + 1,
+      name: newName
+    };
+
+    setPersons(persons.concat(person));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={newNameOnSubmit}>
+        <input value={newName} onChange={nameOnChange} />
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <Person key={person.id} person={person} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let count = 0;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App count={count} />
+)
